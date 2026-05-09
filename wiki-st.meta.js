51145ca880972dba195d22813f11d250f2ad1736
Nux <egil@wp.pl>
2026-05-09 14:30:00 +0200

    commons support, minor

--- wiki-st.meta.js
+++ wiki-st.meta.js
@@ -1,14 +1,15 @@
 // ==UserScript==
-// @name         Wiki-ST
+// @name         Wiki-ST (short title)
 // @namespace    pl.enux.wiki
-// @version      2025-11-10.1
-// @description  Sprzątanie Tytułu. Usuwa m.in. dopisek po kresce z tytułów oraz skraca przestrzenie nazw.
+// @version      2026-05-09.1
+// @description  Short Title aka Sprzątanie Tytułu. Usuwa m.in. dopisek po kresce z tytułów oraz skraca przestrzenie nazw.
 // @author       Nux
 // @match        https://pl.wikipedia.org/*
 // @match        https://en.wikipedia.org/*
 // @match        https://pl.wikimedia.org/*
+// @match        https://commons.wikimedia.org/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=wikipedia.org
 // @grant        none
-// @run-at       document-idle
+// @run-at       document-body
 // @noframes
 // ==/UserScript==
